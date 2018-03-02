6c62e70d0508c474895ca8b943702c9dc6d2f4d0
M.Chan <mo@lxooo.com>
2018-03-02 11:02:08 +0800

    Update & Fix Bugs.

--- config/hashid.php
+++ config/hashid.php
@@ -4,6 +4,9 @@ return [
     'hashid' => [
         'length'     => env('HASHID_LENGTH', 8),
         'salt'       => env('HASHID_SALT', 3.14159265359),
-        'dictionary' => env('HASHID_DICTIONARY', 'hK2VOPI1UHkF6lj9n8L73JgbtSpyAeYrZcwDWqdB0XNGazsv4R5mfExiouTMQC')
+        'dictionary' => env(
+            'HASHID_DICTIONARY',
+            'hK2VOPI1UHkF6lj9n8L73JgbtSpyAeYrZcwDWqdB0XNGazsv4R5mfExiouTMQC'
+        )
     ]
 ];
